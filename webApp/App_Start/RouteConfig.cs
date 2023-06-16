c09799046734283cd029068a927ea8a1a28005ca
STEVE10MR <jm2019063316@virtual.upt.pe>
2023-06-16 10:57:42 -0500

    repair

--- webApp/App_Start/RouteConfig.cs
+++ webApp/App_Start/RouteConfig.cs
@@ -25,7 +25,7 @@ namespace webApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
